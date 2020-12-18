05c31fbf247b8117db5e305e2a1c5f994d772969
csubj <cj@csubj.io>
2020-12-17 23:03:04 -0800

    formatting

--- src/components/Rooms/Rooms.tsx
+++ src/components/Rooms/Rooms.tsx
@@ -7,25 +7,25 @@ type Props = {
     rooms: Array<string>
 }
 
-const Rooms: React.FC<Props> = ({rooms}) => {
-       const layout = [
-        {i: 'a', x: 0, y: 0, w: 1, h: 2},
-        {i: 'b', x: 1, y: 0, w: 2, h: 2},
-        {i: 'c', x: 4, y: 0, w: 1, h: 2}
-      ];
+const Rooms: React.FC<Props> = ({ rooms }) => {
+    const layout = [
+        { i: 'a', x: 0, y: 0, w: 1, h: 2 },
+        { i: 'b', x: 1, y: 0, w: 2, h: 2 },
+        { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+    ];
     return (
-      <ol>
-        {rooms.map((room) => (
-          <li>
-              <div className="maybe">{room}</div>
-              <GridLayout className="layout" layout={layout} cols={24} rowHeight={20} width={1200} resizeHandles={['w' , 'e']}>
-                <div key="a">ken</div>
-                <div key="b">kelly</div>
-                <div key="c">tony</div>
-            </GridLayout>
-            </li>
-        ))}
-      </ol>
+        <ol>
+            {rooms.map((room) => (
+                <li>
+                    <div className="maybe">{room}</div>
+                    <GridLayout className="layout" layout={layout} cols={24} rowHeight={20} width={1200} resizeHandles={['w', 'e']}>
+                        <div key="a">ken</div>
+                        <div key="b">kelly</div>
+                        <div key="c">tony</div>
+                    </GridLayout>
+                </li>
+            ))}
+        </ol>
     );
 }
 
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,27 +2,27 @@ import React, { ReactElement, useState } from "react"
 import TabTitle from "./TabTitle"
 
 type Props = {
-  children: ReactElement[]
+    children: ReactElement[]
 }
 
 const Tabs: React.FC<Props> = ({ children }) => {
-  const [selectedTab, setSelectedTab] = useState(0)
+    const [selectedTab, setSelectedTab] = useState(0)
 
-  return (
-      <div>
-      <ol className="tab-list">
-        {children.map((item, index) => (
-          <TabTitle
-            key={index}
-            title={item.props.title}
-            index={index}
-            setSelectedTab={setSelectedTab}
-          />
-        ))}
-      </ol>
-      {children[selectedTab]}
-    </div>
-  )
+    return (
+        <div>
+            <ol className="tab-list">
+                {children.map((item, index) => (
+                    <TabTitle
+                        key={index}
+                        title={item.props.title}
+                        index={index}
+                        setSelectedTab={setSelectedTab}
+                    />
+                ))}
+            </ol>
+            {children[selectedTab]}
+        </div>
+    )
 }
 
 export default Tabs
